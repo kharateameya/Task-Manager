c600bfbe86c4a3ab5ca041b5c5ee65919216f2cf
kharateameya <kharateameya@gmail.com>
2022-09-12 19:27:21 +0530

    Adding Task Manager project file to git

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require ("express");
+const app = express();
+const tasks = require ("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config()
+const notFound = require("./middleware/not-found")
+const errorHandlerMiddleware = require("./middleware/error-handler")
+//middleware
+app.use(express.static("./public"))
+app.use(express.json())
+
+//routes
+
+
+app.use("/api/v1/tasks", tasks)
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+
+// app.get('/api/v1/tasks')                    get all the tasks
+// app.post('/api/v1/tasks')                   create a new task
+//app.get('/api/v1/tasks/:id')                 -get single task
+// app.patch('/api/v1/tasks/:id')              -update task
+// app.delete('/api/v1/tasks/:id')             -delete task
+                                         
+                                         
+                                       
+                                       
+                                       
+
+const port = process.env.PORT || 3000
+
+const start = async () =>{
+try {
+    app.listen(port, console.log(`server is running on port ${port}...` ))
+    await connectDB(process.env.MONGO_URI)
+
+} catch (error) {
+    console.log(error);
+}
+}
+
+start()
